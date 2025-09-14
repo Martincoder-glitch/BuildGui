522034228df07c2e5a02a23113c4e2450f3c1fbb
Martincoder-glitch <Martinsasuke20241@outlook.com>
2025-09-14 17:16:41 +0100

    Add files via upload

--- digital_clock.py
+++ digital_clock.py
@@ -6,12 +6,16 @@ root.geometry("500x500")
 root.title("Digital clock")
 root.config(background="maroon")
 colors=["cornflower blue","lemon chiffon","Violet Red4","gainsboro"]
+
+    
 def show_time():
-    string=strftime("%H:%M:%S")
+    string=strftime("%A/%m/%y\n%H:%M:%S")
+    root.config(background=colors[random.randint(0,3)])
+
     Cal.config(text=string)
     root.config(background=colors[random.randint(0,3)])
     Cal.after(1000,show_time)
 Cal=Label(root,text="Clock",fg="royal blue",bg="MediumOrchid4",font=("times",28,"bold"))
-Cal.grid(row=1,column=2)
+Cal.grid(row=1,column=3)
 show_time()
 root.mainloop()
